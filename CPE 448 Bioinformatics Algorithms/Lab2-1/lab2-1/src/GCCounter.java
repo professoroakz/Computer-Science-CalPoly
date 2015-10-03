3dca08eee822b5c991badcb78933365c82199e72
Oktay Gardener <oktayb@kth.se>
2015-10-03 11:59:13 -0700

    finish ui and change model-controller

--- CPE 448 Bioinformatics Algorithms/Lab2-1/lab2-1/src/GCCounter.java
+++ CPE 448 Bioinformatics Algorithms/Lab2-1/lab2-1/src/GCCounter.java
@@ -4,52 +4,66 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 
+/* TODO:
+ * I think the GUI class has to be the mother class and the 
+ * child has to be the model as in the GC-count file
+ * I'm not sure if I would have time to take a look tonight. 
+ * but I can look at whatever's there tomorrow
+ * So we basically create a constructor for the child class and 
+ * instansiate it and use the methods we need
+ * GCCount.calcGC(inputfile) or something
+ * And that's an action listener for "Calculate" button
+ * GCCount.calcGC(windowsize, framesize, inputfile) or something
+ *  */
+
 public class GCCounter {
-	
-	public static void main(String args[]) {
+
 		File file;
 		Scanner input = new Scanner(System.in);
 		String seq = "";
 		
-		int gcCount = 0;
-		int nCount = 0;
 		int windowSize;
 		int stepSize;
 		
-		System.out.println("Input the full file name for the test file. "
-				+ "Example: test.FASTA or test.txt");
-		file = new File(input.nextLine());
-		
-		System.out.println("Input the window size for this test.");
-		windowSize = input.nextInt();
-		
-		System.out.println("Input the step size for this test.");
-		stepSize = input.nextInt();
-		
-		try {
-			Scanner scan = new Scanner(file);
-			scan.nextLine();
-			while(scan.hasNextLine()) {
-				seq += scan.nextLine();				
-			}
-		}
-		catch(FileNotFoundException e) {
-			System.out.println("File " + args[0] + " is missing.");
+		public GCCounter(){
+			int gcCount = 0;
+			int nCount = 0;
 		}
 		
-		for(int i = 0; i < seq.length(); i++) {
-			if(seq.charAt(i) == 'G' || seq.charAt(i) == 'C' || 
-					seq.charAt(i) == 'g' || seq.charAt(i) == 'c') {
-				gcCount++;
+		public void readFile(){
+			//System.out.println("Input the full file name for the test file. "
+				//	+ "Example: test.FASTA or test.txt");
+			file = new File(input.nextLine());
+			
+			try {
+				Scanner scan = new Scanner(file);
+				scan.nextLine();
+				while(scan.hasNextLine()) {
+					seq += scan.nextLine();				
+				}
 			}
-			if(seq.charAt(i) == 'n' || seq.charAt(i) == 'N') {
-				nCount++;
+			catch(FileNotFoundException e) {
+				System.out.println("File " + " is missing.");
 			}
 		}
 		
-		DecimalFormat df = new DecimalFormat("#.#");
-		String formatted = df.format(((double)gcCount / (seq.length() - nCount) * 100));
+		public void readGCCount(){
+	
+			for(int i = 0; i < seq.length(); i++) {
+				if(seq.charAt(i) == 'G' || seq.charAt(i) == 'C' || 
+						seq.charAt(i) == 'g' || seq.charAt(i) == 'c') {
+					gcCount++;
+				}
+				if(seq.charAt(i) == 'n' || seq.charAt(i) == 'N') {
+					nCount++;
+				}
+			}
+			
+			DecimalFormat df = new DecimalFormat("#.#");
+			String formatted = df.format(((double)gcCount / (seq.length() - nCount) * 100));
+			
+			System.out.println(formatted + "%");
+		}
 		
-		System.out.println(formatted + "%");
 	}
 }
