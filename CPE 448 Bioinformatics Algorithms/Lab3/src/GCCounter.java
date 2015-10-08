ca0de2873c2e2d9314b8d3df6406950694bfe46b
Oktay Gardener <oktayb@kth.se>
2015-10-08 16:56:50 -0700

    adds lab3

---
+++ CPE 448 Bioinformatics Algorithms/Lab3/src/GCCounter.java
@@ -0,0 +1,172 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+public class GCCounter {
+
+          File file;
+          Scanner scan;
+          PrintWriter writer;
+
+          String seq = "";
+          
+          int windowSize = -1;
+          int stepSize = -1;
+
+          int gcCount;
+          int nCount;
+          
+          public GCCounter(int window, int step) {
+               gcCount = 0;
+               nCount = 0;
+               windowSize = window;
+               stepSize = step;
+          }
+
+          public void readFile(File file) {
+               this.file = file;
+
+               try {
+                    scan = new Scanner(this.file);
+                    scan.nextLine();
+                    while(scan.hasNextLine()) {
+                         seq += scan.nextLine();
+                    }
+               }
+               catch(FileNotFoundException e) {
+                    System.out.println("File " + file.getName() + " is missing.");
+               }
+          }
+           
+          /*
+           * @deprecated Replaced by #readFile(File file).
+           */
+          @Deprecated
+          public void readFile(String fileName){
+               file = new File(fileName);
+               try {
+                    scan = new Scanner(file);
+                    scan.nextLine();
+                    while(scan.hasNextLine()) {
+                         seq += scan.nextLine();                 
+                    }
+               }
+               catch(FileNotFoundException e) {
+                    System.out.println("File " + " is missing.");
+               }
+          }
+          
+          public boolean readBasicGCCount() {
+
+               try {
+                    writer = new PrintWriter("output.txt");
+               }
+               catch(FileNotFoundException e) {
+                    System.out.println("Error when writing to output.txt");
+                    return false; // there was an error, so don't do anything more
+               }
+     
+               try {
+                    for(int i = 0; i < seq.length(); i++) {
+                         checkChar(seq.charAt(i));
+                    }
+               }
+               catch(Exception e) {
+                    System.out.println("Invalid input. Please try another file.");
+                    try {
+                         writer.close();
+                    }
+                    catch(Exception ex) {
+                         // do nothing
+                    }
+                    return false;
+               }
+               
+               writer.println("# of N's present: " + nCount); // Output the total number of N's
+
+               DecimalFormat df = new DecimalFormat("#.#");
+               String formatted = df.format(((double)gcCount / (seq.length() - nCount) * 100));
+               
+               writer.println("%GC overall: " + formatted + "%"); // Output the overall GC %
+
+               return true; // the basic GC succeeds
+          }
+
+          public void readRollingGCCount() {
+               writer.println("Window Size: " + windowSize + ",Step Size: " + stepSize);  // Output the window and step sizes
+               writer.println("Nucleotide Position,%GC");                                 // Output the column headers
+
+               int fullSeqLength = seq.length();
+               int currentLocation = 0;
+               int currentEndLocation = windowSize;
+               String currentSequence;// = seq.substring(currentLocation, currentEndLocation);
+               DecimalFormat df = new DecimalFormat("#.#");
+
+               while(currentEndLocation <= fullSeqLength - 1) {
+                    gcCount = 0;
+                    nCount = 0;
+                    currentSequence = seq.substring(currentLocation, currentEndLocation);
+
+                    try {
+                         for(int i = 0; i < currentSequence.length(); i++) {
+                              checkChar(currentSequence.charAt(i));
+                         }
+                    }
+                    catch(Exception e) {
+                         // do nothing
+                    }
+                    String formatted = df.format(((double)gcCount / (currentSequence.length() - nCount) * 100));
+
+                    writer.println((currentLocation + 1) + "," + formatted);              // Output the nucleotide position and %GC
+                                                                                          // Keep it 1-start as Requirements need
+
+                    currentLocation += stepSize;
+                    currentEndLocation += stepSize;
+               }
+
+               // check if there is still a partial sequence to compute
+               if(currentLocation <= fullSeqLength - 1) {
+                    gcCount = 0;
+                    nCount = 0;
+                    currentSequence = seq.substring(currentLocation);
+
+                    try {
+                         for(int i = 0; i < currentSequence.length(); i++) {
+                              checkChar(currentSequence.charAt(i));
+                         }
+                    }
+                    catch(Exception e) {
+                         // do nothing
+                    }
+
+                    // use separate String variable in case the while-loop is never entered
+                    String formattedPartial = df.format(((double)gcCount / (currentSequence.length() - nCount) * 100));
+
+                    writer.println((currentLocation + 1) + "," + formattedPartial);       // Output the nucleotide position and %GC
+                                                                                          // Keep it 1-start as Requirements need
+               }
+
+               writer.flush();
+               System.out.println("Congratulations! Your file is successfully downloaded to 'output.txt'");
+               try {
+                    writer.close();
+               }
+               catch(Exception e) {
+                    // don't do anything. We just want to close this.
+               }
+          }
+
+          private void checkChar(char currentChar) throws Exception {
+               if(currentChar == 'G' || currentChar == 'C' || currentChar == 'g' || currentChar == 'c') {
+                    gcCount++;
+               }
+               else if(currentChar == 'n' || currentChar == 'N') {
+                    nCount++;
+               }
+               else if(currentChar != 'A' && currentChar != 'a' && currentChar != 'T' && currentChar != 't') {
+                    throw new Exception();
+               }
+          }
+}
---
+++ CPE 448 Bioinformatics Algorithms/Lab3/src/GCCounterView.java
@@ -0,0 +1,127 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JFileChooser;
+import java.io.File;
+
+public final class GCCounterView {
+  private File file;
+  private int windowSize;
+  private int scrollSize;
+     
+  public static void main(String... aArgs){
+    GCCounterView app = new GCCounterView();
+    app.buildAndDisplayGui(); 
+  }
+  
+
+  private void buildAndDisplayGui(){
+    JFrame frame = new JFrame("~the gui~"); 
+    buildContent(frame);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
+  }
+  
+  private void buildContent(JFrame aFrame){
+    final JPanel panel = new JPanel();
+    panel.add(new JLabel("Calculate GC content!"));
+    
+    final JTextField windowSize = new JTextField("Insert window size");
+    windowSize.setHorizontalAlignment(JTextField.CENTER);
+    windowSize.addMouseListener(new MouseAdapter(){
+        @Override
+        public void mouseClicked(MouseEvent e){
+            windowSize.setText("");
+        }
+    });
+
+    final JTextField stepSize = new JTextField("Insert step size");
+    stepSize.setHorizontalAlignment(JTextField.CENTER);
+    stepSize.addMouseListener(new MouseAdapter(){
+        @Override
+        public void mouseClicked(MouseEvent e){
+            stepSize.setText("");
+        }
+    });
+    
+    panel.add(windowSize);
+    panel.add(stepSize);
+    
+    final JTextField chosenFileTextField = new JTextField("Selected file");
+    panel.add(chosenFileTextField);
+   
+    
+    JButton btnFile = new JButton("Select FASTA file");
+    btnFile.addActionListener(new ActionListener() {
+        final JFrame frame = new JFrame("Select file");
+        //Handle open button action.
+        public void actionPerformed(ActionEvent e) {
+            final JFileChooser fc = new JFileChooser(); 
+            int returnVal = fc.showOpenDialog(frame);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                file = fc.getSelectedFile();
+                //This is where a real application would open the file.
+                // System.out.println("File: " + file.getName() + ".");
+                chosenFileTextField.setText(file.getName());
+                // open file 
+                
+                
+            } else {
+                System.out.println("Open command cancelled by user.");
+            }
+            System.out.println(returnVal);
+        }
+    });
+    panel.add(btnFile);
+    
+    JButton ok = new JButton("Calculate!");
+    ok.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            int window = -1;
+            int step = -1;
+            try {
+                window = Integer.parseInt(windowSize.getText());
+                step = Integer.parseInt(stepSize.getText());
+            }
+            catch(NumberFormatException ex) {
+                System.out.println("Either WindowSize or StepSize (or both) is not a number.");
+            }
+
+            if(window != -1 && step != -1) {            
+                // System.out.println("window: " + window + " : step: " + step);
+
+                GCCounter counter = new GCCounter(window, step);
+                counter.readFile(file);
+                // if the readBasicCount function succeeds, then actually do more
+                if(counter.readBasicGCCount()) {
+                    counter.readRollingGCCount();
+                }
+            }
+        }
+    });
+    // ok.addActionListener(new ShowDialog(aFrame));
+    panel.add(ok);
+    
+    aFrame.getContentPane().add(panel);
+  }
+  
+  private static final class ShowDialog implements ActionListener {
+    ShowDialog(JFrame aFrame){
+      fFrame = aFrame;
+    }
+    
+    @Override public void actionPerformed(ActionEvent aEvent) {
+      JOptionPane.showMessageDialog(fFrame, "Success! The file is outputed as output.txt");
+    }
+    private JFrame fFrame;
+  }
+}
+ 
