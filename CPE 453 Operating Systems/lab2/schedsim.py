4e5b97eca3467028b9446950c25328679a37eec4
Oktay Gardener <oktayb@kth.se>
2016-05-03 19:56:45 -0700

    fixes fifo

--- CPE 453 Operating Systems/lab2/schedsim.py
+++ CPE 453 Operating Systems/lab2/schedsim.py
@@ -12,17 +12,44 @@ quantum_rr = -1
 # fix constructs
 # do rr, srjn, fifo etc algorithms
 
+# jobs(job_run_time, arrival_time)
+
 def fifo():
-    process_queue = []
-    n = len(jobs)
-    w_time = 0
-    for i in xrange(n):
-        process_queue.append([])
-        process_queue[i].append(jobs[n][1]) # arrival
+    global jobs
+    joblist = sorted(jobs, key=lambda tup: tup[1])
 
-        w_time += process_queue[i][1]
+    wait_times = []
+    wait_times.append(0) #wt first process is 0
 
-        process_queue[i].append(jobs[n][0]) # burst
+    response_times = []
+    response_times.append(1)
+
+    t_times = [] #turnaround
+
+    total_response_time = 0
+    total_turnaround_time = 0
+    total_wait_time = 0
+
+    for idx, job in enumerate(joblist, start=1):
+        wait_times.append(0)
+        response_times.append(1)
+
+        for idx2, job2 in enumerate(joblist, start=0):
+            if idx == idx2:
+                break
+            wait_times[idx] += job2[0]
+            response_times[idx] = wait_times[idx]
+
+    for idx, job in enumerate(joblist):
+        t_times.append(job[0] + wait_times[idx])
+        print job[0], wait_times[idx]
+        total_wait_time += wait_times[idx]
+        total_turnaround_time += t_times[idx]
+
+    for x in range(0, len(jobs)):
+        print_current_job(jobs[x][1], response_times[x], t_times[x], wait_times[x])
+
+    print_all_jobs(sum(response_times)/len(joblist), total_turnaround_time/len(joblist), total_wait_time/len(joblist))
 
 def srjn():
     pass
@@ -32,10 +59,10 @@ def rr():
 
 
 def print_current_job(job_num, response_time, turnaround_time, wait_time):
-    print('Job:' + '{:3d}'.format(34) + ' -- '
-        + 'Response: ' + '{:3.2f}'.format(3.224)
-        + ' Turnaround: ' + '{:3.2f}'.format(3.224)
-        + ' Wait: ' + '{:3.2f}'.format(3.423))
+    print('Job:' + '{:3d}'.format(job_num) + ' -- '
+        + 'Response: ' + '{:3.2f}'.format(response_time)
+        + ' Turnaround: ' + '{:3.2f}'.format(turnaround_time)
+        + ' Wait: ' + '{:3.2f}'.format(wait_time))
 
 
 def print_all_jobs(response_time, turnaround_time, wait_time):
@@ -44,7 +71,7 @@ def print_all_jobs(response_time, turnaround_time, wait_time):
         + ' Turnaround: ' + '{:3.2f}'.format(turnaround_time)
         + ' Wait: ' + '{:3.2f}'.format(wait_time))
 
-def sched_alg():
+def sched_algorithm(alg):
     if alg is "SRJN":
         srjn()
     elif alg is "RR":
@@ -69,17 +96,19 @@ def check_args(algorithm, quantum):
     return (alg, quantum_rr)
 
 
-def read_input(args, jobs, count):
+def read_input(args):
+    global jobs
     try:
         with open(args.inputfile) as f:
             jobs = [map(int, line.split()) for line in f]
-            return jobs
     except IOError as e:
         print "I/O error({0}): {1}".format(e.errno, e.strerror)
         sys.exit(0)
 
 
 def main():
+    global jobs
+
     parser = argparse.ArgumentParser()
     parser.add_argument('inputfile', help='inputfile')
     parser.add_argument('-p', nargs=1)
@@ -89,7 +118,7 @@ def main():
 
     count = 0
 
-    jobs = read_input(args, jobs, count)
+    read_input(args)
 
     # print("~ Filename: {}".format(args.inputfile))
     # print("~ -p: {}".format(args.p))
@@ -103,6 +132,7 @@ def main():
         print(key, val)
 
     jobs = sorted(jobs, key=lambda tup: tup[1])
+    sched_algorithm(sched_alg)
 
 
 if __name__ == '__main__':
