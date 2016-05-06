93b1b0a5868a709dc33488dabd45f4ca02f29efe
Simon Weigl <weigl0@hm.edu>
2016-05-05 18:33:18 -0700

    Make gateway working

--- CPE 458 Current Topics in Computer Systems - Distributed Systems/Project2/Json Api/Gateway/GatewayHandlers.go
+++ CPE 458 Current Topics in Computer Systems - Distributed Systems/Project2/Json Api/Gateway/GatewayHandlers.go
@@ -8,7 +8,7 @@ import (
     "io"
     "bytes"
     "time"
-    "strconv"
+    // "strconv"
 
     "github.com/gorilla/mux"
 )
@@ -16,16 +16,27 @@ import (
 // curl -H "Content-Type: application/json" -d '{"key":"1-2-06", "value":"bar"}' http://localhost:8080/
 
 const dateFormat = "1-2-06"
-const numNodes = 2
+const numNodes = 3
+var nodes [numNodes]string
+var indexToNodes map[int64][numNodes]int = make(map[int64][3]int)
+
+func init(){
+    nodes[0] = "localhost:8081"
+    nodes[1] = "localhost:8082"
+    nodes[2] = "localhost:8083"
+    indexToNodes[0] = [numNodes]int{0, 1, 2}
+    indexToNodes[1] = [numNodes]int{1, 2, 0}
+    indexToNodes[2] = [numNodes]int{2, 0, 1}
+}
 
 // Takes a date and returns the responsible node
-func GetNode(date string) (node string, err error) {
+func GetNodeIndicies(date string) (nodeIndicies [numNodes]int, err error) {
     t, err := time.Parse(dateFormat, date)
     if err != nil {
         return
     }
     id := t.Unix() / (24*60*60)
-    node = strconv.FormatInt(id % numNodes + 1, 10) // 1 based index
+    nodeIndicies = indexToNodes[id % numNodes]
     err = nil
     return
 }
@@ -42,20 +53,30 @@ func GatewayStockShow(w http.ResponseWriter, r *http.Request){
     key := vars["key"]
     fmt.Println("KEY: " + key)
 
-    node, err := GetNode(key)
+    // Map key to list of 3 responsible nodes
+    responsibleNodes, err := GetNodeIndicies(key)
     if err != nil {
         fmt.Println(w, "Invalid key")
         return
     }
 
-    // Sends get to responsible node
-    fmt.Println("GET http://localhost:808" + node + "/" + key)
-    resp, err := http.Get("http://localhost:808" + node + "/" + key)
+    // Sends get to responsible node/s
+    i := 0
+    fmt.Println("GET http://" + nodes[responsibleNodes[i]] + "/" + key)
+    resp, err := http.Get("http://" + nodes[responsibleNodes[i]] + "/" + key)
+    for err != nil && i < 2 {
+        i = i + 1
+        fmt.Println("GET http://" + nodes[responsibleNodes[i]] + "/" + key)
+        resp, err = http.Get("http://" + nodes[responsibleNodes[i]] + "/" + key)
+    }
+
+    // Found no responsible node
     if err != nil {
-        fmt.Fprintln(w, "GET: Node " + node + " not available")
+        fmt.Fprintln(w, "GET: No responsible node accessable")
         return
     }
     
+    // Read response
     buf := new(bytes.Buffer)
     buf.ReadFrom(resp.Body)
     defer resp.Body.Close()
@@ -65,39 +86,34 @@ func GatewayStockShow(w http.ResponseWriter, r *http.Request){
 func GatewayStockCreate(w http.ResponseWriter, r *http.Request) {
     var stock Stock
 
-    body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
-    if err != nil {
-        panic(err)
-    }
-    if err := r.Body.Close(); err != nil {
-        panic(err)
-    }
-    err = json.Unmarshal(body, &stock)
-    // if err != nil {
-    //     return
-    // }
+    body, _ := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+    r.Body.Close()
 
-    node, err := GetNode(stock.Key)
+    err := json.Unmarshal(body, &stock)
+    if err != nil {
+        fmt.Println("Error while reading Json")
+        return
+    }
+
+    // Map key to list of 3 responsible nodes
+    responsibleNodes, err := GetNodeIndicies(stock.Key)
     if err != nil {
         fmt.Println(w, "Invalid key")
         return
     }
 
     // Sends POST to responsible node
-    var jsonStr = []byte(`{"key":"` + stock.Key + `","value":"` + stock.Value + `"}`)
-    req, err := http.NewRequest("POST", "http://localhost:808" + node, bytes.NewBuffer(jsonStr))
-    req.Header.Set("Content-Type", "application/json")
-
-    client := &http.Client{}
-    resp, err := client.Do(req)
-
-    if err != nil {
-        fmt.Sprintln("POST: Node " + node + " not available")
-        return
+    jsonStr := bytes.NewBuffer([]byte(`{"key":"` + stock.Key + `","value":"` + stock.Value + `"}`))
+    fmt.Println("POST", jsonStr)
+    for i := 0; i < 3; i++ {
+        fmt.Println("POST http://" + nodes[responsibleNodes[i]] + "/" + stock.Key)
+        req, _ := http.NewRequest("POST", "http://" + nodes[responsibleNodes[i]], jsonStr)
+        req.Header.Set("Content-Type", "application/json")
+        client := &http.Client{}
+        _, err := client.Do(req)
+        if err != nil {
+            fmt.Fprintln(w, "POST: Cannot access " + nodes[responsibleNodes[i]])
+            return
+        }
     }
-    
-    buf := new(bytes.Buffer)
-    buf.ReadFrom(resp.Body)
-    defer resp.Body.Close()
-    fmt.Fprintln(w, buf.String())
 }
